8b3ac0e23dfc36e3f069b05a35dcac439cd898cd
Valentin-KV <krivenko.vg@yandex.ru>
2025-10-22 21:49:13 +0300

    feat: complite string task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -422,8 +422,8 @@ function invertCase(str) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -436,8 +436,8 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.replace(/Hello, |!/gi, '');
 }
 
 /**
@@ -451,8 +451,8 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, -1);
 }
 
 /**
@@ -470,8 +470,8 @@ function unbracketTag(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
@@ -490,8 +490,24 @@ function extractEmails(/* str */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const lowerABC = 'abcdefghijklmnopqrstuvwxyz';
+  const upperABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  return str
+    .split('')
+    .map((char) => {
+      if (lowerABC.includes(char)) {
+        const index = lowerABC.indexOf(char);
+        return lowerABC[(index + 13) % 26];
+      }
+      if (upperABC.includes(char)) {
+        const index = upperABC.indexOf(char);
+        return upperABC[(index + 13) % 26];
+      }
+      return char;
+    })
+    .join('');
 }
 
 /**
@@ -518,8 +534,14 @@ function encodeToRot13(/* str */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const deck =
+    "'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'";
+  const arrDeck = deck
+    .split(',')
+    .map((item) => item.trim().replace(/^'|'$/g, ''));
+  const index = arrDeck.findIndex((card) => card === value);
+  return index;
 }
 
 module.exports = {
